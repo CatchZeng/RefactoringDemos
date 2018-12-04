d28b26515562e4af130843f13a4a34bffbb26cbc
CatchZeng <891793848@qq.com>
2018-12-04 17:52:18 +0800

    feat: add demo

---
+++ app/src/main/java/com/catchzeng/refactoringdemos/r3_large_class/LargeClass2.java
@@ -0,0 +1,19 @@
+package com.catchzeng.refactoringdemos.r3_large_class;
+
+public class LargeClass2 {
+
+    class JobItem {
+        double getTotalPrice() {
+            return 1000;
+        }
+
+        double getUintPrice() {
+            return 100;
+        }
+
+        String getEmployee() {
+            return "zhangsan";
+        }
+    }
+
+}
